a388391e1cf6f8a704bf5c4a228fa074969d21cd
dujie <2545251075@qq.com>
2021-07-19 22:54:48 +0800

    rabbitmq SpringBoot初始化

---
+++ rabbitmq-consumer/src/main/java/com/js/receiver/topic/TopicTotalReceiver.java
@@ -0,0 +1,18 @@
+package com.js.receiver.topic;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+
+@Component
+@RabbitListener(queues = "topic.woman")
+public class TopicTotalReceiver {
+
+    @RabbitHandler
+    public void process(Map testMessage) {
+        System.out.println("TopicWomenReceiver消费者收到消息  : " + testMessage.toString());
+    }
+}
